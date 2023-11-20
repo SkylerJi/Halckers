ea6e12bfbe6c6308842c2bb22bcce14283c76160
SkylerJi <skylerji@gmail.com>
2023-11-20 10:44:43 -0500

    added  the names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,21 @@ export default function Home() {
   return (
     <div>
       Halckers, yo
+      <ul>
+        <li>Adi Patro</li>
+        <li>Ali Raza</li>
+        <li>Dean Petev</li>
+        <li>Dhruv Agarwal</li>
+        <li>Joe Rosenberg</li>
+        <li>Justin Xu</li>
+        <li>Kesava Viswanadha</li>
+        <li>Praveen Dharmavarapu</li>
+        <li>Shane Liu</li>
+        <li>Sid Das</li>
+        <li>Skyler Ji</li>
+        <li>Sri Jaladi</li>
+        <li>Yusuf Hacking</li>
+      </ul>
     </div>
   )
 }
