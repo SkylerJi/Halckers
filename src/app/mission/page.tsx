f4a0c1731c3b528ad811c9dd4f287be4f1dca4a1
SkylerJi <skylerji@gmail.com>
2023-12-03 03:57:36 -0800

    i hate frontend so much

--- src/app/mission/page.tsx
+++ src/app/mission/page.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image"
 export default function mission(){
     return (
-        <div>
-            Halckers LLC is a nonprofit meant to spread peace, love, and joy across the world.
-
-        </div>
+        <div className="flex flex-col items-center justify-center">
+      <p className="text-center mb-4">
+        Halckers LLC is a nonprofit meant to spread peace, love, and joy across the world.
+      </p>
+    
+    </div>
     )
 }
