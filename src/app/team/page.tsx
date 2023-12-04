11cc10e994aa0e27f4f2f1d83d1fb15a503a93a3
SkylerJi <skylerji@gmail.com>
2023-12-04 09:51:43 -0800

    new descriptions

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -32,7 +32,7 @@ export default function team(){
         <div className="relative h-96 border-8 border-double border-black bg-white overflow-hidden">
         <div className="absolute inset-0 flex items-center justify-center bg-white opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out z-10">
             <div className="px-4 py-2 font-mono">
-                As the Chief Footbol Officer, Ali's loyalty and heart lie with Ronaldo.
+                As the Chief Footbol Officer, similar to many 7 year olds across the world, Ali&apos;s loyalty and heart lie with Ronaldo.
               
             </div>
           </div>
