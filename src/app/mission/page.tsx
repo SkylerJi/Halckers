a156d46efd9a89b0dadc6b913f176bd715108f67
SkylerJi <skylerji@gmail.com>
2023-12-04 00:29:27 -0800

    hi

--- src/app/mission/page.tsx
+++ src/app/mission/page.tsx
@@ -6,7 +6,7 @@ export default function mission(){
        <div className = "w-screen h-screen p-24 items-center text-center leading-10">
         <h1 className = "text-5xl md:text-9xl font-mono my-16">Who we are</h1>
         <span className = "font-mono text-3xl md:text-5xl">
-          We are an elite group of cyber warriors dedicated to protecting the innocent and defending the ether.
+          We are an <span className="font-black">elite</span> group of cyber warriors dedicated to <span className="font-black">protecting</span> the innocent and <span className="font-black">defending</span> the ether.
         </span>
       
        </div>
@@ -14,29 +14,33 @@ export default function mission(){
         <h1 className = "text-5xl md:text-9xl font-mono my-16">
           What we did
         </h1>
-        <span className = "font mono text-xl md:text-3xl">
-          Founded in 2017, we took on the role of cyberpatriots. 
+        <span className = "font mono text-xl md:text-5xl ">
+          Founded in 2017, we became <span className="font-bold">Cyberpatriots. </span>
           In the contest to save Missouri, we took home gold. 
-          But that wasn&apos;t enough to end cyber crime. 
-          We took our talents internationally, saving the Internet of Things at the highest competitive level. 
+          <div className = "my-12">
+
+          </div>
+          But that wasn&apos;t enough to <span className="line-through">end</span> cyber crime. 
+          We took our talents internationally, <span className="font-bold">saving</span> the Internet of Things at the highest competitive level. 
         </span>
 
        </div>
       
-       <div className = "w-screen h-screen bg-white text-black items-center text-center leading-10 p-24">
+       <div className = "w-screen h-screen bg-white text-black items-center text-center leading-10 py-24 px-12">
         <h1 className = "text-5xl md:text-9xl font-mono my-16">
           What we do
         </h1>
-        <span className = "font mono text-xl md:text-3xl">
-          After defeating cyber crime once and for all, we disbanded...is what those who prey on the weak wish we did.
+        <span className = "font mono text-xl md:text-5xl">
+          After <span className="font-bold">defeating</span> cyber crime once and for all, we disbanded...is what those who prey on the weak wish we did.
          
         </span>
-        <div className = "my-6">
+        <div className = "my-12">
 
         </div>
-        <span className = "font mono text-xl md:text-3xl">
-         Through our state of the art Integrated Mass Ecosystem Service Spanning Across Global Economies, or iMessage system, we are able to communicate effectively, efficiently, and erratically.
+        <span className="font mono text-xl md:text-5xl">
+            Through our <span className="font-bold">state-of-the-art</span> Integrated Mass Ecosystem Service Spanning Across Global Economies, or iMessage system, we are able to communicate effectively, efficiently, and <span className="font-bold">erratically</span>.
         </span>
+
       
 
        </div>
