1b5e29b1f86b03b65416d32dd26478ba1effcd6c
SkylerJi <skylerji@gmail.com>
2023-12-03 23:34:58 -0800

    fixed grammar

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -230,7 +230,7 @@ export default function team(){
         <div className="relative h-96 border-8 border-double border-black bg-white overflow-hidden">
         <div className="absolute inset-0 flex items-center justify-center bg-white opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out z-10">
             <div className="px-4 py-2 font-mono">
-                As the Chief South-Indian Officer, Yusuf identifies as South Indian despite of his Egyptian background. Truly inspiring. It is important to note that he DOES NOT identify as North Indian. Mashallah.
+                As the Chief South-Indian Officer, Yusuf identifies as South Indian despite his Egyptian background. Truly inspiring. It is important to note that he DOES NOT identify as North Indian. Mashallah.
               
             </div>
           </div>
