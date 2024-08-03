fb89a4f69c91e9e62e3b4611b6dd94c5d09279be
SkylerJi <skylerji@gmail.com>
2024-08-02 22:16:25 -0700

    trying

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    swcMinify: true,
+    // Disable SWC for arm64 mac (M1/M2) in development
+    ...(process.env.NODE_ENV === 'development' && process.platform === 'darwin' && process.arch === 'arm64'
+      ? { swcMinify: false }
+      : {}),
+  }
+  
+  module.exports = nextConfig
